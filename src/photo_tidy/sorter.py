da5ccd3fdc79d179c57fe54266accc71f53bba42
J Gulan <jg23497@gmail.com>
2025-06-19 06:46:05 +0100

    Create a preprocessors module.

--- src/photo_tidy/sorter.py
+++ src/photo_tidy/sorter.py
@@ -7,7 +7,7 @@ import shutil
 from photo_tidy.reporting.report import Report
 from photo_tidy.reporting.move_report_item import MoveReportItem
 from photo_tidy.reporting.skipped_report_item import SkippedReportItem
-from .whatsapp_preprocessor import WhatsAppPreprocessor
+from photo_tidy.preprocessors.whatsapp_preprocessor import WhatsAppPreprocessor
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
